b2fa3be0f45be062e556f2f20b23edf83955cc7b
digberi <orkepuma@gmail.com>
2021-04-08 18:16:15 +0300

    Fix a problem with a non-existent file

--- data/dbms.js
+++ data/dbms.js
@@ -2,19 +2,24 @@ const fs = require('fs')
 const util = require('util')
 const parser = require('../parser')
 
+const pathToDB = 'data/results/results.txt'
+
 const readFile = util.promisify(fs.readFile)
 const writeFile = util.promisify(fs.writeFile)
 
-const writeData = (data) => writeFile('data/results/results.txt', data)
-const readData = () => readFile('data/results/results.txt', 'utf-8')
+const writeData = (data) => writeFile(pathToDB, data)
+const readData = () => readFile(pathToDB, 'utf-8')
 
 const getResults = async () => {
   const object = {
     results: [],
   }
-  await readData().then((data) => {
-    object.results = data.split('\n').map((el) => parseFloat(el))
-  })
+  if (fs.existsSync(pathToDB)) {
+    await readData().then((data) => {
+      object.results = data.split('\n').map((el) => parseFloat(el))
+    })
+  }
+
   return object
 }
 
